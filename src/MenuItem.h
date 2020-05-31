a81df18deb80bfb6b2076dbcde3aa3d170feb25e
forntoh <thomasforntoh@gmail.com>
2020-05-31 11:32:25 +0100

    added getters and setters for MenuItem

--- src/MenuItem.h
+++ src/MenuItem.h
@@ -8,10 +8,25 @@ class MenuItem {
     MenuItem* subMenu = NULL;
 
    public:
+    //
+    // constructors
+    //
     MenuItem(char* text) : text(text) {}
     MenuItem(char* text, void (*callback)()) : text(text), callback(callback) {}
     MenuItem(char* text, void (*callback)(), MenuItem* subMenu)
         : text(text), callback(callback), subMenu(subMenu) {}
+    //
+    // getters
+    //
+    char* getText() { return text; }
+    void* getCallback() { return callback; }
+    MenuItem* getSubMenu() { return subMenu; }
+    //
+    // setters
+    //
+    void setText(char* text) { this->text = text; }
+    void setCallBack(void (*callback)()) { this->callback = callback; }
+    void setSubMenu(MenuItem* subMenu) { this->subMenu = subMenu; }
 };
 
 #endif
