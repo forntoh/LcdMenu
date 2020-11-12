d09fbeff6c0c03cf4042c7eb1958b1f8a70b6c64
forntoh <thomasforntoh@gmail.com>
2020-11-12 21:30:36 +0100

    fixed warning: ISO C++ forbids converting a string constant to 'char*'

--- src/MenuItem.h
+++ src/MenuItem.h
@@ -44,7 +44,7 @@ const byte MENU_ITEM_END_OF_MENU = 7;
 //
 class MenuItem {
    private:
-    char* text = "";
+    char* text = (char*)"";
     fptr callback = NULL;
     MenuItem* subMenu = NULL;
     byte type = MENU_ITEM_NONE;
@@ -81,8 +81,8 @@ class MenuItem {
     //
     //
     boolean isOn = false;
-    char* textOn = "ON";
-    char* textOff = "OFF";
+    char* textOn = (char*)"ON";
+    char* textOff = (char*)"OFF";
     String value = "";
 };
 
