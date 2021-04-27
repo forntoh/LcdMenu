d2b143ee43fd0807fd89644244ab476388716752
Forntoh Thomas <thomasforntoh@gmail.com>
2021-04-27 16:04:37 +0100

    succeded with level 03

--- src/test copy.cpp
+++ src/test copy.cpp
@@ -9,24 +9,67 @@ void setup() {
     Serial.begin(9600);
     while (!Serial) continue;
 
+    // char input[] =
+    //     "002\n032Settings\n070Backlight\n160Contrast\n132WiFi\n050User\n152"
+    //     "Android01\n";
+
+    // MenuItem* nextMenu;
+    // MenuItem* currMenu;
+
+    // String line;
+    // while ((line = readLine(input)).length() > 0) {
+    //     uint8_t pos = line.charAt(0) - '0';
+    //     uint8_t type = line.charAt(1) - '0';
+    //     uint8_t size = line.charAt(2) - '0';
+    //     if (type == 0) {
+    //         currMenu = creatMenu(size, NULL);
+    //     } else if (type == MENU_ITEM_SUB_MENU) {
+    //         nextMenu = creatMenu(size, currMenu);
+    //         currMenu[pos + 1] = ItemSubMenu(line.substring(3), nextMenu);
+    //     } else {
+    //         nextMenu[pos + 1] = MenuItem(line.substring(3));
+    //     }
+    // }
+
     char input[] =
-        "032Settings\n070Backlight\n160Contrast\n132WiFi\n050User\n132Android01"
-        "\n050Password\n140Connect\n";
+        "0020\n0320Settings\n1320WiFi\n0701Backlight\n1601Contrast\n0501User\n1"
+        "321Android\n0502Password\n1402Connect\n";
 
-    MenuItem* nextMenu;
+    uint8_t step = 0;
+    uint8_t prevPos;
 
-    MenuItem* currMenu = creatMenu(2, NULL);
+    MenuItem* currMenu;
 
     String line;
     while ((line = readLine(input)).length() > 0) {
         uint8_t pos = line.charAt(0) - '0';
+        uint8_t type = line.charAt(1) - '0';
         uint8_t size = line.charAt(2) - '0';
+        uint8_t i = line.charAt(3) - '0';
 
-        if (line.charAt(1) - '0' == MENU_ITEM_SUB_MENU) {
-            nextMenu = creatMenu(size, currMenu);
-            currMenu[pos + 1] = ItemSubMenu(line.substring(3), nextMenu);
+        // Serial.println(step, DEC);
+
+        if (type == 0) {
+            currMenu = creatMenu(size, NULL);
+        } else if (type == MENU_ITEM_SUB_MENU) {
+            prevPos = pos + 1;
+
+            if (i == 1) {
+                currMenu[step].getSubMenu()[prevPos] =
+                    ItemSubMenu(line.substring(4), creatMenu(size, currMenu));
+            } else
+                currMenu[pos + 1] =
+                    ItemSubMenu(line.substring(4), creatMenu(size, currMenu));
+
         } else {
-            nextMenu[pos + 1] = MenuItem(line.substring(3));
+            if (pos == 0) ++step;
+
+            if (i == 2) {
+                currMenu[step - 1].getSubMenu()[prevPos].getSubMenu()[pos + 1] =
+                    MenuItem(line.substring(4));
+            } else
+                currMenu[step].getSubMenu()[pos + 1] =
+                    MenuItem(line.substring(4));
         }
     }
 
