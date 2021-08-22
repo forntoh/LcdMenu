0a9abb1f852abe165c920b8457a9aec273479ace
Forntoh Thomas <thomasforntoh@gmail.com>
2021-08-22 23:49:07 +0200

    removed generate menu

--- src/MenuItem.h
+++ src/MenuItem.h
@@ -3,7 +3,7 @@
 
   MIT License
 
-  Copyright (c) 2020 Forntoh Thomas
+  Copyright (c) 2020-2021 Forntoh Thomas
 
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files (the "Software"), to deal
