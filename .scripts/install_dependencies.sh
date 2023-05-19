f8cfd5abd81a9cf173c96edda9cc6e912f0fe6bb
forntoh <thomasforntoh@gmail.com>
2023-05-20 01:07:10 +0200

    ci change

--- .scripts/install_dependencies.sh
+++ .scripts/install_dependencies.sh
@@ -1,1 +1,11 @@
-pio lib --storage-dir $(bundle exec arduino_library_location.rb) install marcoschwartz/LiquidCrystal_I2C@^1.1.4
+# Example build script
+bundle install
+
+# ensure the Arduino installation -- creates the Library directory
+bundle exec ensure_arduino_installation.rb
+
+# manually install a custom library from a git repository
+git clone https://github.com/johnrickman/LiquidCrystal_I2C.git
+mv LiquidCrystal_I2C $(bundle exec arduino_library_location.rb)
+
+# pio lib --storage-dir $(bundle exec arduino_library_location.rb) install marcoschwartz/LiquidCrystal_I2C@^1.1.4
