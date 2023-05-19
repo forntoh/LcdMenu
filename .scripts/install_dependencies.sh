888780f81cbf21d5ddaeb27f5f6bc1cfd8a82b04
forntoh <thomasforntoh@gmail.com>
2023-05-20 01:59:08 +0200

    gem file

--- .scripts/install_dependencies.sh
+++ .scripts/install_dependencies.sh
@@ -8,4 +8,4 @@ bundle exec ensure_arduino_installation.rb
 git clone https://github.com/johnrickman/LiquidCrystal_I2C.git
 mv LiquidCrystal_I2C $(bundle exec arduino_library_location.rb)
 
-# pio lib --storage-dir $(bundle exec arduino_library_location.rb) install marcoschwartz/LiquidCrystal_I2C@^1.1.4
+bundle exec arduino_ci.rb --skip-examples-compilation
