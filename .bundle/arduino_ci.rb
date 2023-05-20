3c3cd8f9938ea45db1b6f65eb77de076f1649e8a
forntoh <thomasforntoh@gmail.com>
2023-05-20 04:13:09 +0200

    delet

--- .bundle/arduino_ci.rb
+++
@@ -1,4 +0,0 @@
-source 'https://rubygems.org'
-
-# Replace 1.2 with the desired version of arduino_ci.  See https://guides.rubygems.org/patterns/#pessimistic-version-constraint
-gem 'arduino_ci', '~> 1.5.0'
