f529f9e0945756c025d15561ae0573be09c44fa8
forntoh <thomasforntoh@gmail.com>
2024-09-10 21:33:15 +0200

    Update examples to use SimpleNavConfig
    - Updated examples to utilize the SimpleNavConfig struct for navigation configuration.
    - Replaced individual key definitions with a single struct for menu navigation in each example.

--- examples/CharsetInput/CharsetInput.ino
+++ examples/CharsetInput/CharsetInput.ino
@@ -8,39 +8,40 @@
 #include <ItemInput.h>
 #include <LcdMenu.h>
 #include <interface/LiquidCrystalI2CAdapter.h>
-#include <utils/commandProccesors.h>
+#include <utils/SimpleNavConfig.h>
 
 #define LCD_ROWS 2
 #define LCD_COLS 16
 
-// Configure keyboard keys (ASCII)
-#define UP 'w'
-#define DOWN 's'
-#define LEFT 'a'
-#define RIGHT 'd'
-#define ENTER ' '
-#define BACK 'b'
-#define BACKSPACE 'v'
-#define CLEAR 'c'
-
-#define CHARSET_SIZE 10
 // Create your charset
 char charset[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
-// Active index of the charset
-int8_t charsetPosition = -1;
 
 // Declare the call back function
-void inputCallback(char* value);
+void inputCallback(char *value);
 
 MAIN_MENU(
     ITEM_INPUT("Con", inputCallback),
     ITEM_BASIC("Connect to WiFi"),
     ITEM_BASIC("Blink SOS"),
-    ITEM_BASIC("Blink random")
-);
+    ITEM_BASIC("Blink random"));
 
 LiquidCrystalI2CAdapter lcdAdapter(0x27, LCD_COLS, LCD_ROWS);
 LcdMenu menu(lcdAdapter);
+
+SimpleNavConfig navConfig = {
+    .menu = &menu,
+    .up = 'w',
+    .down = 's',
+    .enter = ' ',
+    .back = 'b',
+    .left = 'a',
+    .right = 'd',
+    .clear = 'c',
+    .backspace = 'v',
+    .charset = charset,
+    .charsetSize = 10,
+    .charsetPosition = -1,
+};
 
 void setup() {
     Serial.begin(9600);
@@ -50,13 +51,12 @@ void setup() {
 void loop() {
     if (!Serial.available()) return;
     char command = Serial.read();
-    processMenuCommand(menu, command, charsetPosition, charset, CHARSET_SIZE,
-                       UP, DOWN, ENTER, BACK, CLEAR, BACKSPACE, LEFT, RIGHT);
+    processWithSimpleCommand(&navConfig, command);
 }
 /**
  * Define callback
  */
-void inputCallback(char* value) {
+void inputCallback(char *value) {
     // Do stuff with value
     Serial.print(F("# "));
     Serial.println(value);
--- examples/SimpleInput/SimpleInput.ino
+++ examples/SimpleInput/SimpleInput.ino
@@ -8,33 +8,34 @@
 #include <ItemInput.h>
 #include <LcdMenu.h>
 #include <interface/LiquidCrystalI2CAdapter.h>
-#include <utils/commandProccesors.h>
+#include <utils/SimpleNavConfig.h>
 
 #define LCD_ROWS 2
 #define LCD_COLS 16
 
-// Configure keyboard keys (ASCII)
-#define UP 56        // NUMPAD 8
-#define DOWN 50      // NUMPAD 2
-#define LEFT 52      // NUMPAD 4
-#define RIGHT 54     // NUMPAD 6
-#define ENTER 53     // NUMPAD 5
-#define BACK 55      // NUMPAD 7
-#define BACKSPACE 8  // BACKSPACE
-#define CLEAR 46     // NUMPAD .
-
 // Declare the call back function
-void inputCallback(char* value);
+void inputCallback(char *value);
 
 MAIN_MENU(
-    ITEM_INPUT("Con", inputCallback), 
+    ITEM_INPUT("Con", inputCallback),
     ITEM_BASIC("Connect to WiFi"),
-    ITEM_BASIC("Blink SOS"), 
-    ITEM_BASIC("Blink random")
-);
+    ITEM_BASIC("Blink SOS"),
+    ITEM_BASIC("Blink random"));
 
 LiquidCrystalI2CAdapter lcdAdapter(0x27, LCD_COLS, LCD_ROWS);
 LcdMenu menu(lcdAdapter);
+
+SimpleNavConfig navConfig = {
+    .menu = &menu,
+    .up = 'w',
+    .down = 's',
+    .enter = ' ',
+    .back = 'b',
+    .left = 'a',
+    .right = 'd',
+    .clear = 'c',
+    .backspace = 'v',
+};
 
 void setup() {
     Serial.begin(9600);
@@ -44,13 +45,15 @@ void setup() {
 void loop() {
     if (!Serial.available()) return;
     char command = Serial.read();
-    processMenuCommand(menu, command, UP, DOWN, LEFT, RIGHT, ENTER, BACK, CLEAR,
-                       BACKSPACE);
+
+    if (!processWithSimpleCommand(&navConfig, command) && command != '\n') {
+        menu.type(command);
+    }
 }
 /**
  * Define callback
  */
-void inputCallback(char* value) {
+void inputCallback(char *value) {
     Serial.print(F("# "));
     Serial.println(value);
 }
