12013cd311604d28ad9224f32600ead2a16d0bbe
Dmitry Shishkin <dimoninax@gmail.com>
2024-09-19 01:17:14 +0200

    Remove types

--- src/utils/constants.h
+++ src/utils/constants.h
@@ -6,19 +6,6 @@ typedef void (*fptrInt)(uint16_t);
 typedef void (*fptrStr)(char*);
 typedef char* (*fptrMapping)(uint16_t);
 //
-// menu item types
-//
-const byte MENU_ITEM_MAIN_MENU_HEADER = 1;
-const byte MENU_ITEM_SUB_MENU_HEADER = 2;
-const byte MENU_ITEM_SUB_MENU = 3;
-const byte MENU_ITEM_COMMAND = 4;
-const byte MENU_ITEM_INPUT = 5;
-const byte MENU_ITEM_NONE = 6;
-const byte MENU_ITEM_TOGGLE = 7;
-const byte MENU_ITEM_END_OF_MENU = 8;
-const byte MENU_ITEM_LIST = 9;
-const byte MENU_ITEM_PROGRESS = 10;
-//
 // Control codes
 //
 #define BACKSPACE 8  // Backspace
