761a33a76b7e963e75dac5dde6732eb1d3e86e60
Dmitry Shishkin <dimoninax@gmail.com>
2024-09-18 22:13:18 +0200

    Add ItemBack

---
+++ examples/ItemBack/ItemBack.ino
@@ -0,0 +1,58 @@
+/*
+ Sub Menu
+
+ https://lcdmenu.forntoh.dev/examples/submenu
+
+*/
+#include <ItemSubMenu.h>
+#include <ItemBack.h>
+#include <LcdMenu.h>
+#include <display/LiquidCrystal_I2CAdapter.h>
+#include <utils/SimpleNavConfig.h>
+
+#define LCD_ROWS 2
+#define LCD_COLS 16
+
+extern MenuItem* settingsMenu[];
+
+// Define the main menu
+MAIN_MENU(
+    ITEM_BASIC("Start service"),
+    ITEM_BASIC("Connect to WiFi"),
+    ITEM_SUBMENU("Settings", settingsMenu),
+    ITEM_BASIC("Blink SOS"),
+    ITEM_BASIC("Blink random"));
+/**
+ * Create submenu and precise its parent
+ */
+SUB_MENU(
+    settingsMenu,
+    mainMenu,
+    ITEM_BASIC("Backlight"),
+    ITEM_BASIC("Contrast"),
+    ITEM_BACK());
+
+LiquidCrystal_I2C lcd(0x27, LCD_COLS, LCD_ROWS);
+LiquidCrystal_I2CAdapter lcdAdapter(&lcd, LCD_COLS, LCD_ROWS);
+LcdMenu menu(lcdAdapter);
+
+SimpleNavConfig navConfig = {
+    .menu = &menu,
+    .up = 'w',
+    .down = 's',
+    .enter = ' ',
+    .back = 'b',
+    .left = NULL,
+    .right = NULL,
+};
+
+void setup() {
+    Serial.begin(9600);
+    menu.initialize(mainMenu);
+}
+
+void loop() {
+    if (!Serial.available()) return;
+    char command = Serial.read();
+    processWithSimpleCommand(&navConfig, command);
+}
