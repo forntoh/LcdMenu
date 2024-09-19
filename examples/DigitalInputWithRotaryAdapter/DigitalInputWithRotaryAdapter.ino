49c2787cecd252079cdc0d47985f928279e92da2
forntoh <thomasforntoh@gmail.com>
2024-09-19 02:07:29 +0200

    Add Digital Input Adapter for Enhanced Menu Navigation
    This commit introduces the following changes:
    
    1. **Digital Input Adapter:**
       - Added a new `DigitalInputAdapter` class to handle various digital inputs for menu navigation.
       - Supports multiple types of digital inputs including buttons, switches, and other digital sensors.
       - Provides debouncing functionality for stable readings.
    
    2. **Example Usage:**
       - Provided example sketch (`DigitalInputAdapter.ino`) demonstrating the usage of various digital inputs with the `LcdMenu`.
       - Added another example sketch (`DigitalInputWithRotaryAdapter.ino`) showing how `DigitalInputAdapter` can be used in conjunction with other input adapters like `SimpleRotaryAdapter`. This example demonstrates that double press actions, which are often not read properly, can be substituted with a dedicated button for more reliable input.
    
    These changes enhance the flexibility and usability of the menu system by allowing various digital inputs to interact seamlessly with the menu.

---
+++ examples/DigitalInputWithRotaryAdapter/DigitalInputWithRotaryAdapter.ino
@@ -0,0 +1,52 @@
+#include <ItemList.h>
+#include <ItemToggle.h>
+#include <LcdMenu.h>
+#include <SimpleRotary.h>
+#include <display/LiquidCrystal_I2CAdapter.h>
+#include <input/DigitalInputAdapter.h>
+#include <input/SimpleRotaryAdapter.h>
+
+// Disable double press detection
+#define DOUBLE_PRESS_THRESHOLD 0
+
+#define LCD_ROWS 2
+#define LCD_COLS 16
+
+// Declare the callbacks
+void colorsCallback(uint16_t pos);
+void toggleBacklight(uint16_t isOn);
+
+String colors[] = {"Red", "Green", "Blue", "Orange", "Aqua", "Yellow", "Purple", "Pink"};
+
+MAIN_MENU(
+    ITEM_BASIC("Connect to WiFi"),
+    ITEM_STRING_LIST("Color", colors, 8, colorsCallback),
+    ITEM_BASIC("Blink SOS"),
+    ITEM_TOGGLE("Backlight", toggleBacklight),
+    ITEM_BASIC("Blink random"));
+
+LiquidCrystal_I2C lcd(0x27, LCD_COLS, LCD_ROWS);
+LiquidCrystal_I2CAdapter lcdAdapter(&lcd, LCD_COLS, LCD_ROWS);
+LcdMenu menu(lcdAdapter);
+SimpleRotary encoder(2, 3, 4);
+SimpleRotaryAdapter rotaryInput(&menu, &encoder);       // Rotary input adapter
+DigitalInputAdapter backspaceBtn(&menu, 5, BACKSPACE);  // Push button for backspace
+
+void setup() {
+    Serial.begin(9600);
+    menu.initialize(mainMenu);
+}
+
+void loop() {
+    rotaryInput.observe();
+    backspaceBtn.observe();
+}
+
+// Define the callbacks
+void toggleBacklight(uint16_t isOn) {
+    lcdAdapter.setBacklight(isOn);
+}
+
+void colorsCallback(uint16_t pos) {
+    Serial.println(colors[pos]);
+}
