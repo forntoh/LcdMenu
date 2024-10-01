a0148b5f20f50de43d8915c31f2cb1a56ba40a93
forntoh <thomasforntoh@gmail.com>
2024-10-01 18:34:57 +0200

    Update script calls to use Python 3.

--- docs.sh
+++ docs.sh
@@ -1,9 +1,9 @@
 doxygen docs/Doxyfile
 
-python3 .scripts/link_doxygen_sphinx.py
+python .scripts/link_doxygen_sphinx.py
 
-python3 .scripts/link_examples_sphinx.py
+python .scripts/link_examples_sphinx.py
 
-python3 .scripts/update_changelog.py
+python .scripts/update_changelog.py
 
-python3 .scripts/append_includes.py docs/source/reference/migration desc false
+python .scripts/append_includes.py docs/source/reference/migration desc false
