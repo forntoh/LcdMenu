afcee11575874c8fd584b1e2f29b3a0f327bb488
forntoh <thomasforntoh@gmail.com>
2024-10-05 18:41:51 +0200

    MenuRendered docs

--- src/renderer/MenuRenderer.h
+++ src/renderer/MenuRenderer.h
@@ -5,6 +5,14 @@
 #include <Arduino.h>
 #include <utils/utils.h>
 
+/**
+ * @class MenuRenderer
+ * @brief Abstract base class for rendering a menu on a display.
+ *
+ * This class provides the basic structure and functionality for rendering a menu
+ * on a display. It includes methods for drawing items, moving the cursor, and
+ * handling edit mode.
+ */
 class MenuRenderer {
   protected:
     uint8_t maxCols;
@@ -23,40 +31,85 @@ class MenuRenderer {
     unsigned long startTime = 0;
 
   public:
+    /**
+     * Pointer to the display interface used for rendering.
+     */
     DisplayInterface* display;
 
-    MenuRenderer(
-        DisplayInterface* display,
-        uint8_t maxCols,
-        uint8_t maxRows) : maxCols(maxCols), maxRows(maxRows), display(display) {}
+    /**
+     * @brief Constructor for MenuRenderer.
+     * @param display Pointer to the display interface.
+     * @param maxCols Maximum number of columns in the display.
+     * @param maxRows Maximum number of rows in the display.
+     */
+    MenuRenderer(DisplayInterface* display, uint8_t maxCols, uint8_t maxRows)
+        : maxCols(maxCols), maxRows(maxRows), display(display) {}
 
+    /**
+     * @brief Initializes the display and starts the timer.
+     */
     virtual void begin() {
         display->begin();
         startTime = millis();
     }
 
+    /**
+     * @brief Function to draw a byte on the display.
+     * @param byte The byte to be drawn.
+     */
     virtual void draw(uint8_t byte) = 0;
 
+    /**
+     * @brief Draws an item on the display.
+     * @param itemIndex Index of the item to be drawn.
+     * @param screenRow Row on the screen where the item should be drawn.
+     * @param text Text of the item to be drawn.
+     */
     virtual void drawItem(uint8_t itemIndex, uint8_t screenRow, const char* text) {
         this->cursorRow = screenRow;
         this->itemIndex = itemIndex;
     }
 
+    /**
+     * @brief Function to draw the cursor on the display.
+     */
     virtual void drawCursor() = 0;
 
+    /**
+     * @brief Function to clear the blinker from the display.
+     */
     virtual void clearBlinker() = 0;
 
+    /**
+     * @brief Function to draw the blinker on the display.
+     */
     virtual void drawBlinker() = 0;
 
+    /**
+     * @brief Moves the cursor to a specified position.
+     * @param cursorCol Column position to move the cursor to.
+     * @param cursorRow Row position to move the cursor to.
+     */
     virtual void moveCursor(uint8_t cursorCol, uint8_t cursorRow) {
         this->cursorCol = cursorCol;
         this->cursorRow = cursorRow;
     }
 
+    /**
+     * @brief Pure virtual function to clear the cursor from the display.
+     */
     virtual void clearCursor() = 0;
 
+    /**
+     * @brief Sets the total number of items in the menu.
+     * @param itemCount Total number of items.
+     */
     void setItemCount(uint8_t itemCount) { this->itemCount = itemCount; }
 
+    /**
+     * @brief Sets the edit mode for the menu.
+     * @param inEditMode Flag indicating whether to enter or exit edit mode.
+     */
     void setEditMode(bool inEditMode) {
         if (this->inEditMode != inEditMode) {
             this->inEditMode = inEditMode;
@@ -66,11 +119,17 @@ class MenuRenderer {
         }
     }
 
+    /**
+     * @brief Restarts the display timer and shows the display.
+     */
     virtual void restartTimer() {
         this->startTime = millis();
         display->show();
     }
 
+    /**
+     * @brief Updates the display timer and hides the display if the timeout is reached.
+     */
     virtual void updateTimer() {
         if (millis() != startTime + DISPLAY_TIMEOUT) {
             return;
@@ -79,16 +138,40 @@ class MenuRenderer {
         display->hide();
     }
 
+    /**
+     * @brief Checks if the menu is in edit mode.
+     * @return True if in edit mode, false otherwise.
+     */
     bool isInEditMode() const { return inEditMode; }
 
+    /**
+     * @brief Gets the current column position of the cursor.
+     * @return Current column position of the cursor.
+     */
     uint8_t getCursorCol() const { return cursorCol; }
 
+    /**
+     * @brief Gets the current row position of the cursor.
+     * @return Current row position of the cursor.
+     */
     uint8_t getCursorRow() const { return cursorRow; }
 
+    /**
+     * @brief Gets the maximum number of rows in the display.
+     * @return Maximum number of rows.
+     */
     uint8_t getMaxRows() const { return maxRows; }
 
+    /**
+     * @brief Gets the maximum number of columns in the display.
+     * @return Maximum number of columns.
+     */
     uint8_t getMaxCols() const { return maxCols; }
 
+    /**
+     * @brief Gets the index of the current item.
+     * @return Index of the current item.
+     */
     uint8_t getItemIndex() const { return itemIndex; }
 };
 
