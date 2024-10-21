c84b5ee38081c03cf66b3fb1ddb47b6840338922
forntoh <thomasforntoh@gmail.com>
2024-10-21 11:20:58 +0200

    Add BaseWidgetValue class for widgets with values.
    This commit introduces a new header file defining the BaseWidgetValue class, which serves as a base class for widgets holding values. It includes methods to retrieve and set values, draw the widget, process menu commands, and handle value changes through callbacks.

---
+++ src/widget/BaseWidgetValue.h
@@ -0,0 +1,61 @@
+// Creator: @ShishkinDmitriy
+#ifndef BASE_WIDGET_WITH_VALUE_H
+#define BASE_WIDGET_WITH_VALUE_H
+
+#include "BaseWidget.h"
+
+class LcdMenu;
+
+/**
+ * @class BaseWidgetValue
+ * @brief Base class for widget holding some value.
+ */
+template <typename T>
+class BaseWidgetValue : public BaseWidget {
+    template <typename T0, typename T1, typename T2>
+    friend class Item;
+
+  protected:
+    T value;
+    const char* format = nullptr;
+    void (*callback)(T) = nullptr;
+
+  public:
+    BaseWidgetValue(T value, const char* format, const uint8_t blinkerOffset = 0, void (*callback)(T) = nullptr)
+        : BaseWidget(blinkerOffset), value(value), format(format), callback(callback) {}
+    /**
+     * @brief Retrieve current value.
+     */
+    T getValue() const { return value; }
+    /**
+     * @brief Sets the value.
+     *
+     * @param newValue The value to set.
+     * @note You need to call `LcdMenu::refresh` after this method to see the changes.
+     */
+    virtual void setValue(const T newValue) { value = newValue; }
+
+  protected:
+    /**
+     * @brief Draw the widget into specified buffer.
+     *
+     * @param buffer the buffer where widget will be drawn
+     * @param start the index where to start drawing in the buffer
+     */
+    uint8_t draw(char* buffer, const uint8_t start) override {
+        return snprintf(buffer + start, ITEM_DRAW_BUFFER_SIZE, format, value);
+    }
+
+    bool process(LcdMenu* menu, unsigned char command) override = 0;
+
+    void handleChange() {
+        if (callback != nullptr) {
+            callback(value);
+        }
+    }
+
+  public:
+    ~BaseWidgetValue() override = default;
+};
+
+#endif
