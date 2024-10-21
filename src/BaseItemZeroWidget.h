45a466e0244f2c2b913316e0cbd36d89c175b23d
forntoh <thomasforntoh@gmail.com>
2024-10-21 11:34:39 +0200

    Add BaseItemZeroWidget class for custom commit handling
    This commit introduces a new class, BaseItemZeroWidget, which extends MenuItem and provides a framework for menu items that require custom or no commit handling. It requires derived classes to implement handleCommit for specific commit actions.

---
+++ src/BaseItemZeroWidget.h
@@ -0,0 +1,44 @@
+// Creator: @ShishkinDmitriy
+#ifndef BASE_ITEM_ZERO_WIDGET_H
+#define BASE_ITEM_ZERO_WIDGET_H
+
+#include "MenuItem.h"
+
+/**
+ * @class BaseItemZeroWidget
+ * @brief A base class for menu items that require a custom or no commit handling.
+ *
+ * This class extends the MenuItem class and provides a framework for menu items
+ * that either need no commit handling or need to handle commit actions in a specific way.
+ * It requires derived classes to implement the handleCommit for custom commit handling.
+ *
+ * @note This class is intended to be used as a base class for other menu items.
+ *       It should not be instantiated directly.
+ */
+class BaseItemZeroWidget : public MenuItem {
+  public:
+    /**
+     * @brief Construct a new BaseItemZeroWidget object.
+     *
+     * @param text The text to display for the menu item.
+     */
+    explicit BaseItemZeroWidget(const char* text) : MenuItem(text) {}
+
+  protected:
+    virtual void handleCommit(LcdMenu* menu) = 0;
+
+    void draw(MenuRenderer* renderer) override {
+        renderer->drawItem(text, nullptr);
+    }
+
+    bool process(LcdMenu* menu, const unsigned char command) override {
+        if (command == ENTER) {
+            handleCommit(menu);
+            printLog(F("ItemWidget::enter"), text);
+            return true;
+        }
+        return false;
+    }
+};
+
+#endif
