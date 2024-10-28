f03ca9671246a2def5025a4a8c821c562bf26b61
forntoh <thomasforntoh@gmail.com>
2024-10-29 00:18:54 +0100

    Update ButtonWithRotaryAdapter Example
    - Added ItemWidget.h and WidgetList.h
    - Replaced ITEM_STRING_LIST with ITEM_WIDGET for colors

--- examples/ButtonWithRotaryAdapter/ButtonWithRotaryAdapter.ino
+++ examples/ButtonWithRotaryAdapter/ButtonWithRotaryAdapter.ino
@@ -3,8 +3,8 @@
 
 #include <Button.h>
 #include <ItemInputCharset.h>
-#include <ItemList.h>
 #include <ItemToggle.h>
+#include <ItemWidget.h>
 #include <LcdMenu.h>
 #include <MenuScreen.h>
 #include <SimpleRotary.h>
@@ -12,21 +12,24 @@
 #include <input/ButtonAdapter.h>
 #include <input/SimpleRotaryAdapter.h>
 #include <renderer/CharacterDisplayRenderer.h>
+#include <widget/WidgetList.h>
 
 #define LCD_ROWS 2
 #define LCD_COLS 16
 
 // Declare the callbacks
-void colorsCallback(uint8_t pos);
 void toggleBacklight(bool isOn);
 void inputCallback(char* value);
 
-String colors[] = {"Red", "Green", "Blue", "Orange", "Aqua", "Yellow", "Purple", "Pink"};
+const char* colors[] = {"Red", "Green", "Blue", "Orange", "Aqua", "Yellow", "Purple", "Pink"};
 
 // clang-format off
 MENU_SCREEN(mainScreen, mainItems,
     ITEM_INPUT_CHARSET("User", (const char*)"ABCDEFGHIJKLMNOPQRSTUVWXYZ", inputCallback),
-    ITEM_STRING_LIST("Color", colors, 8, colorsCallback),
+    ITEM_WIDGET(
+        "Color",
+        [](const char* color) { Serial.println(color); },
+        WIDGET_LIST(colors, 8, 0, "%s")),
     ITEM_TOGGLE("Backlight", toggleBacklight),
     ITEM_BASIC("Placeholder 1"),
     ITEM_BASIC("Placeholder 2"));
@@ -56,10 +59,6 @@ void loop() {
 // Define the callbacks
 void toggleBacklight(bool isOn) {
     lcdAdapter.setBacklight(isOn);
-}
-
-void colorsCallback(uint8_t pos) {
-    Serial.println(colors[pos]);
 }
 
 void inputCallback(char* value) {
