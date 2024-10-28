a4595869f7ddde2e0fabff8d9847bc0bf5729bed
forntoh <thomasforntoh@gmail.com>
2024-10-29 00:16:09 +0100

    Update List Example
    - Replace ItemList with ItemWidget
    - Update string arrays to const char*
    - Modify menu items to use WidgetList

--- examples/List/List.ino
+++ examples/List/List.ino
@@ -1,33 +1,29 @@
-#include <ItemList.h>
+#include <ItemWidget.h>
 #include <LcdMenu.h>
 #include <MenuScreen.h>
 #include <display/LiquidCrystal_I2CAdapter.h>
 #include <input/KeyboardAdapter.h>
 #include <renderer/CharacterDisplayRenderer.h>
+#include <widget/WidgetList.h>
 
 #define LCD_ROWS 2
 #define LCD_COLS 16
 
-// Declare the callbacks
-void colorsCallback(uint8_t pos);
-void numsCallback(uint8_t pos);
-
-// Declare the array
-extern String colors[];
-// Initialize the array
-String colors[] = {"Red", "Green", "Blue", "Orange", "Aqua", "Yellow", "Purple", "Pink"};
-
-// Declare the array
-extern String nums[];
-// Initialize the array
-String nums[] = {"5", "7", "9", "12", "32"};
+const char* colors[] = {"Red", "Green", "Blue", "Orange", "Aqua", "Yellow", "Purple", "Pink"};
+const char* nums[] = {"5", "7", "9", "12", "32"};
 
 // Initialize the main menu items
 // clang-format off
 MENU_SCREEN(mainScreen, mainItems,
     ITEM_BASIC("List demo"),
-    ITEM_STRING_LIST("Col", colors, 8, colorsCallback),
-    ITEM_STRING_LIST("Num", nums, 5, numsCallback),
+    ITEM_WIDGET(
+        "Color",
+        [](const char* color) { Serial.println(color); },
+        WIDGET_LIST(colors, 8, 0, "%s")),
+    ITEM_WIDGET(
+        "Num",
+        [](const char* num) { Serial.println(num); },
+        WIDGET_LIST(nums, 5, 0, "%s")),
     ITEM_BASIC("Example"));
 // clang-format on
 
@@ -45,15 +41,4 @@ void setup() {
 
 void loop() {
     keyboard.observe();
-}
-
-// Define the callbacks
-void colorsCallback(uint8_t pos) {
-    // do something with the index
-    Serial.println(colors[pos]);
-}
-
-void numsCallback(uint8_t pos) {
-    // do something with the index
-    Serial.println(nums[pos]);
 }
