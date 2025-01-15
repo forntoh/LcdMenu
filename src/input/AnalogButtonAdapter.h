d8fa5dae2d28686e5ccb62915735f5b8d67df71c
scratchbit <matejmrkva@seznam.cz>
2025-01-15 12:26:10 +0100

    Rewrite of adapter

--- src/input/AnalogButtonAdapter.h
+++ src/input/AnalogButtonAdapter.h
@@ -19,40 +19,49 @@
  * @param pinNumber is analog pin where is connected the array of buttons
  */
 
-//Center value for each button
-#define btn_val_RIGHT 20	// BACKSPACE
-#define btn_val_UP 100		// UP
-#define btn_val_DOWN 250	// DOWN
-#define btn_val_LEFT 400	// BACK
-#define btn_val_SELECT 640	// ENTER
-
-#define btn_MARGIN 20		// Margin of defined value
-#define btn_val_DEFAULT 1000	// Value when no button is pressed
-#define btn_pressTime 300	// Last time the button was pressed
+class ButtonConfig {
+  public:
+	static constexpr int16_t DEFAULT_VALUE = 1000;
+	static constexpr unsigned long PRESS_TIME_MS = 300;
+	static constexpr uint16_t DEFAULT_MARGIN = 20;
+};
 
 class AnalogButtonAdapter : public InputInterface {
   private:
-    uint8_t pinNumber;
+    uint16_t pinNumber;
+    uint16_t triggerValue;
+    uint16_t margin;
+    byte command;
     unsigned long lastPressTime = 0;  // Last time the button was pressed
     
   public:
-    AnalogButtonAdapter(LcdMenu* menu, uint8_t pinNumber)
-        : InputInterface(menu), pinNumber(pinNumber) {
+    AnalogButtonAdapter(LcdMenu* menu, uint8_t pinNumber, uint16_t triggerValue, uint16_t margin, byte command)
+        : InputInterface(menu), pinNumber(pinNumber), triggerValue(triggerValue), margin(margin), command(command){
+    }
+    AnalogButtonAdapter(LcdMenu* menu, uint8_t pinNumber, uint16_t triggerValue, byte command)
+        : InputInterface(menu), pinNumber(pinNumber), triggerValue(triggerValue), margin(ButtonConfig::DEFAULT_MARGIN), command(command){
     }
 
     void observe() override {
-
-        int16_t ar = analogRead(pinNumber);	//Read value from pin
+	// Read analog value from pin
+        int16_t analogValue = analogRead(pinNumber);	//Read value from pin
+        
+        // Ignore readings above default value (no button pressed)
+    	if (analogValue >= ButtonConfig::DEFAULT_VALUE) {
+        	return;
+     	}
+     	
+        // Apply debouncing
         unsigned long currentTime = millis();
-        if (ar < btn_val_DEFAULT) {     	//Check if the button is pressed
-        	if(currentTime - lastPressTime > btn_pressTime){	//Check if 
-        	        lastPressTime = currentTime;
-        		if (ar < (btn_val_RIGHT + btn_MARGIN) && ar > (btn_val_RIGHT-btn_MARGIN)) menu->process(BACKSPACE); 
-        		else if (ar < (btn_val_UP + btn_MARGIN) && ar > (btn_val_UP-btn_MARGIN))  menu->process(UP);
-        		else if (ar < (btn_val_DOWN + btn_MARGIN) && ar > (btn_val_DOWN-btn_MARGIN))  menu->process(DOWN);
-        		else if (ar < (btn_val_LEFT + btn_MARGIN) && ar > (btn_val_LEFT-btn_MARGIN)) menu->process(BACK);
-        		else if (ar < (btn_val_SELECT + btn_MARGIN) && ar > (btn_val_SELECT-btn_MARGIN))  menu->process(ENTER);  
-        	}
-    	}
+        if (currentTime - lastPressTime <= ButtonConfig::PRESS_TIME_MS){
+        	return;
+        }
+        
+        // Process button press
+        lastPressTime = currentTime;
+        
+        if (analogValue < (triggerValue+margin) && analogValue > (triggerValue-margin)){
+        	menu->process(command);
+        }
     }
 };
