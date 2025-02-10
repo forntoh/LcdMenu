9f8052ca387b0ae960b083939e7949099fb42af5
Badn1nja <57497096+Badn1nja@users.noreply.github.com>
2025-02-10 10:17:02 +0000

    Update pcf8574_i2c.ino
    Correct Classes and Add defines for ease

--- examples/pcf8574_i2c/pcf8574_i2c.ino
+++ examples/pcf8574_i2c/pcf8574_i2c.ino
@@ -8,6 +8,10 @@
 // PFC8574 I2C Screen
 //
 //
+#define I2C_ADDR 0x27
+#define LCD_COLS 16
+#define LCD_ROWS 2
+
 // clang-format off
 MENU_SCREEN(mainScreen, mainItems,
     ITEM_BASIC("Start service"),
@@ -17,9 +21,9 @@ MENU_SCREEN(mainScreen, mainItems,
     ITEM_BASIC("Blink random"));
 // clang-format on
 
-LCD_I2C lcd(0x27, 16, 2);
+LCD_I2C lcd(I2C_ADDR,LCD_COLS,LCD_ROWS);
 LCD_I2CAdapter lcdAdapter(&lcd);
-CharacterDisplayRenderer renderer(&lcdAdapter, 20, 4, 0xDF, 0xE1);
+CharacterDisplayRenderer renderer(&lcdAdapter, LCD_COLS, LCD_ROWS);
 LcdMenu menu(renderer);
 KeyboardAdapter keyboard(&menu, &Serial);
 
