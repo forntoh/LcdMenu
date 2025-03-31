60ff371a0b014ebbf29aa1bcc304faeb12c1cb08
forntoh <thomasforntoh@gmail.com>
2025-03-31 14:45:14 +0200

    Replace StandardCplusplus library with ArduinoSTL in project configuration and source files

--- src/utils/std.h
+++ src/utils/std.h
@@ -2,7 +2,7 @@
 
 #if defined(__has_include)
 #if !__has_include(<vector>)
-#include <StandardCplusplus.h>
+#include <ArduinoSTL.h>
 #endif
 #else
 // Fallback: for compilers that don’t support __has_include,
@@ -10,6 +10,6 @@
     !defined(ARDUINO_ARCH_ESP8266) && \
     !defined(ARDUINO_ARCH_STM32) &&   \
     !defined(ARDUINO_ARCH_SAMD)
-#include <StandardCplusplus.h>
+#include <ArduinoSTL.h>
 #endif
 #endif
