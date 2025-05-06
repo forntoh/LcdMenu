8516ce3010fe49dfeb5fb22edc37cdb27ee90b9c
Teemo Vaas <teemo@digiwood.ee>
2025-05-06 22:11:50 +0300

    Float printing for STM32
    Remove STM32 from the list of known to have full sprintf support, because apparently testing shows on bluePill there is no float support by default.

--- src/utils/custom_printf.h
+++ src/utils/custom_printf.h
@@ -3,7 +3,7 @@
 #ifndef USE_CUSTOM_PRINTF
 // Platforms known to have full sprintf support
 #if defined(ARDUINO_ARCH_ESP32) || defined(ARDUINO_ARCH_ESP8266) || \
-    defined(ARDUINO_ARCH_STM32) || defined(ARDUINO_ARCH_SAMD)
+    defined(ARDUINO_ARCH_SAMD)
 #define USE_CUSTOM_PRINTF 0
 #else
 #define USE_CUSTOM_PRINTF 1
