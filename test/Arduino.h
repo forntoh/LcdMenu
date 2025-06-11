a2f9d1e062724e59b9463ca7f0ceeeb9f782bd49
Thomas Forntoh <thomasforntoh@gmail.com>
2025-06-11 21:15:11 +0200

    Fix Arduino stub

--- test/Arduino.h
+++ test/Arduino.h
@@ -2,7 +2,16 @@
 #define ARDUINO_H
 #include <stdint.h>
 #include <stddef.h>
+#include <string.h>
 using byte = uint8_t;
 using boolean = bool;
 inline unsigned long millis() { return 0; }
+template <typename T, typename A, typename B>
+T constrain(T val, A low, B high) {
+    if (val < static_cast<T>(low)) return static_cast<T>(low);
+    if (val > static_cast<T>(high)) return static_cast<T>(high);
+    return val;
+}
+struct __FlashStringHelper {};
+#define F(x) x
 #endif
