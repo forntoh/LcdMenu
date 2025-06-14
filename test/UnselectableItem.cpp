1ca19143fd01a519dab7dc472a89b42635a9cb93
Thomas Forntoh <thomasforntoh@gmail.com>
2025-06-15 00:08:35 +0200

    Fix memory leaks in label tests

--- test/UnselectableItem.cpp
+++ test/UnselectableItem.cpp
@@ -31,7 +31,10 @@ class DummyRenderer : public MenuRenderer {
 };
 
 unittest(cursor_skips_unselectable_items) {
-    std::vector<MenuItem*> items = {ITEM_LABEL("Title"), ITEM_BASIC("First"), ITEM_BASIC("Second")};
+    MenuItem* label = ITEM_LABEL("Title");
+    MenuItem* first = ITEM_BASIC("First");
+    MenuItem* second = ITEM_BASIC("Second");
+    std::vector<MenuItem*> items = {label, first, second};
     MenuScreen screen(items);
     DummyRenderer renderer;
     screen.setCursor(&renderer, 0);
@@ -40,16 +43,27 @@ unittest(cursor_skips_unselectable_items) {
     assertEqual(2, screen.getCursor());
     screen.up(&renderer);
     assertEqual(1, screen.getCursor());
+    delete label;
+    delete first;
+    delete second;
 }
 
 unittest(cursor_down_skips_multiple_labels) {
-    std::vector<MenuItem*> items = {ITEM_BASIC("A"), ITEM_LABEL("Label1"), ITEM_LABEL("Label2"), ITEM_BASIC("B")};
+    MenuItem* a = ITEM_BASIC("A");
+    MenuItem* l1 = ITEM_LABEL("Label1");
+    MenuItem* l2 = ITEM_LABEL("Label2");
+    MenuItem* b = ITEM_BASIC("B");
+    std::vector<MenuItem*> items = {a, l1, l2, b};
     MenuScreen screen(items);
     DummyRenderer renderer;
     screen.setCursor(&renderer, 0);
     assertEqual(0, screen.getCursor());
     screen.down(&renderer);
     assertEqual(3, screen.getCursor());
+    delete a;
+    delete l1;
+    delete l2;
+    delete b;
 }
 
 unittest_main()
