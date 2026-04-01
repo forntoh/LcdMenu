c3e444fa860423e041a81abd4d3ec58af99d58e3
forntoh <thomasforntoh@gmail.com>
2026-04-02 00:13:27 +0200

    chore: remove legacy utils and constants shim headers

--- src/utils/constants.h
+++
@@ -1,6 +0,0 @@
-#ifndef LCD_MENU_CONSTANTS_LEGACY_H
-#define LCD_MENU_CONSTANTS_LEGACY_H
-
-#include "lcd_menu_constants.h"
-
-#endif  // LCD_MENU_CONSTANTS_LEGACY_H
--- src/utils/utils.h
+++
@@ -1,6 +0,0 @@
-#ifndef LCD_MENU_UTILS_LEGACY_H
-#define LCD_MENU_UTILS_LEGACY_H
-
-#include "lcd_menu_utils.h"
-
-#endif  // LCD_MENU_UTILS_LEGACY_H
